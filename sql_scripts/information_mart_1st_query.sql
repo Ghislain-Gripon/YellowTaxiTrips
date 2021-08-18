4bc4d3647dc0f92445f34d6e60c9119a6e3da82e
Ghislain-Gripon <ghislain.gripon@protonmail.com>
2021-08-18 21:01:49 +0200

    script correted for corelated subquery error

--- sql_scripts/information_mart_1st_query.sql
+++ sql_scripts/information_mart_1st_query.sql
@@ -3,102 +3,38 @@ CREATE TABLE information_mart.trips_statistics_month_most_trips_per_year AS
 	SELECT 
 		puzone.zonename AS "Pickup Zone Name", 
 		dozone.zonename AS "Dropoff Zone Name", 
-		(SELECT TO_CHAR(
-	    	TO_DATE (CAST(EXTRACT(MONTH FROM td.timestring) AS TEXT), 'MM'), 'Month'
-	    	)
-		) 
-		AS "Month Name",
-		(SELECT 
-	    	EXTRACT(YEAR FROM td.timestring)
-		)
-		AS "Year",
+		TO_CHAR(TO_DATE (CAST(EXTRACT(MONTH FROM td.timestring) AS TEXT), 'MM'), 'Month') AS "Month Name",
+	    EXTRACT(YEAR FROM td.timestring) AS "Year",
+
 		avg(total_amount) AS "Average Total Amount",
-		
-		(SELECT 
-			PERCENTILE_CONT(0.75) 
-		WITHIN GROUP 
-			(ORDER BY total_amount)
-		FROM business_vault.tripfacts) 
-		AS "3rd Quartile Total Amount",
-		(SELECT 
-			PERCENTILE_CONT(0.5) 
-		WITHIN GROUP 
-			(ORDER BY total_amount)
-		FROM business_vault.tripfacts) 
-		AS "Median Total Amount",
-		
-		(SELECT 
-			PERCENTILE_CONT(0.25) 
-		WITHIN GROUP 
-			(ORDER BY total_amount)
-		FROM business_vault.tripfacts)
-		AS "1st Quartile Total Amount",
-		
+		PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY total_amount) AS "3rd Quartile Total Amount", 
+		PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY total_amount) AS "Median Total Amount",
+		PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY total_amount) AS "1st Quartile Total Amount",
 		min(total_amount) AS "Minimum Total Amount", 
 		max(total_amount) AS "Maximum Total Amount", 
 		sum(total_amount) AS "Sum Total Amount",
+
 		count(*) AS "Count", 
+
 		avg(passengercount) AS "Average Passsenger Count", 
 		sum(passengercount) AS "Total Number of Passenger",
+
 		sum(trip_distance) AS "Total Trip Distance",
 		avg(trip_distance) AS "Average Trip Distance",
-		
-		(SELECT 
-			PERCENTILE_CONT(0.75) 
-		WITHIN GROUP 
-			(ORDER BY total_amount)
-		FROM business_vault.tripfacts) 
-		AS "3rd Quartile Trip Distance",
-		
-		(SELECT 
-			PERCENTILE_CONT(0.5) 
-		WITHIN GROUP 
-			(ORDER BY total_amount)
-		FROM business_vault.tripfacts) 
-		AS "Median Trip Distance",
-		
-		(SELECT 
-			PERCENTILE_CONT(0.25) 
-		WITHIN GROUP 
-			(ORDER BY total_amount)
-		FROM business_vault.tripfacts) 
-		AS "1st Quartile Trip Distance",
-		
+
+		PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY trip_distance) AS "3rd Quartile Trip Distance",
+		PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY trip_distance) AS "Median Trip Distance",
+		PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY trip_distance) AS "1st Quartile Trip Distance",
 		min(trip_distance) AS "Minimum Trip Distance", 
 		max(trip_distance) AS "Maximum Trip Distance",
-		avg(trip_time) AS "Average Trip Time",
-		
-		(SELECT 
-			PERCENTILE_CONT(0.75) 
-		WITHIN GROUP 
-			(ORDER BY trip_time)
-		FROM business_vault.tripfacts) 
-		AS "3rd Quartile Trip Time",
-		(SELECT 
-			PERCENTILE_CONT(0.5) 
-		WITHIN GROUP 
-			(ORDER BY trip_time)
-		FROM business_vault.tripfacts) 
-		AS "Median Trip Time",
-		
-		(SELECT 
-			PERCENTILE_CONT(0.25) 
-		WITHIN GROUP 
-			(ORDER BY trip_time)
-		FROM business_vault.tripfacts)
-		AS "1st Quartile Trip Time",
-		
-		min(trip_time) AS "Minimum Trip Time", 
-		max(trip_time) AS "Maximum Trip Time", 
-		sum(trip_time) AS "Sum Trip Time",
-		
+
 		(SELECT 
 			rcd.ratecodename
 		FROM business_vault.tripfacts tf 
 			JOIN business_vault.ratecodedim rcd ON tf.ratecodeid = rcd.ratecodeid
 			JOIN business_vault.timedim td ON td.timeid = tf.pickuptimeid
 		GROUP BY 
-			rcd.ratecodeid, 
+			rcd.ratecodename, 
 			EXTRACT(MONTH FROM td.timestring), 
 			EXTRACT(YEAR FROM td.timestring) 
 		ORDER BY 
@@ -112,7 +48,7 @@ CREATE TABLE information_mart.trips_statistics_month_most_trips_per_year AS
 			JOIN business_vault.payment_typedim ptd ON tf.ratecodeid = ptd.payment_typeid 
 			JOIN business_vault.timedim td ON td.timeid = tf.pickuptimeid
 		GROUP BY 
-			ptd.payment_typeid, 
+			ptd.payment_type_name, 
 			EXTRACT(MONTH FROM td.timestring), 
 			EXTRACT(YEAR FROM td.timestring) 
 		ORDER BY 
